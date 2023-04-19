c681ade7a7d016c131bfef0e757f4744a16d28c1
Squizel <iustin224@gmail.com>
2023-04-20 02:56:56 +0300

    Update contextMenu.cpp

--- src/contextMenu.cpp
+++ src/contextMenu.cpp
@@ -1,9 +1,20 @@
 #include "contextMenu.hpp"
+#include "box.hpp"
+#include "stack.hpp"
 
 using namespace squi;
 
 ContextMenu::operator Child() const {
     auto storage = std::make_shared<Storage>();
     
-    return Child(std::make_shared<Widget>(this->widget));
+    return Stack{
+        .children{
+            Box{
+                .widget{
+                    .width = 100.f,
+                    .height = 100.f,
+                },
+            },
+        },
+    };
 }
