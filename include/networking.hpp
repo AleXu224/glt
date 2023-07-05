ab2ddaf7539525f32b3a71c86f528059efe60c55
Squizel <iustin224@gmail.com>
2023-07-05 23:00:04 +0300

    Add support for chunked transfers

--- include/networking.hpp
+++ include/networking.hpp
@@ -1,33 +1,33 @@
 #pragma once
 
 #include <unordered_map>
+#define WIN32_LEAN_AND_MEAN
 #define ASIO_STANDALONE
 #include "asio.hpp"
 #include "vector"
 
 namespace squi {
-    class Networking {
-        static asio::io_context ioContext;
-        static std::vector<std::thread> threads;
+	class Networking {
+		static asio::io_context ioContext;
+		static std::vector<std::thread> threads;
 
-        struct ResponseBody {
-            std::string body;
-            uint32_t statusCode;
-            std::unordered_map<std::string, std::string> headers;
-        };
-
-        static ResponseBody parseResponse(std::string_view response);
-
-    public:
-
-        struct Response {
+		struct ResponseBody {
 			std::string body;
-            uint32_t statusCode;
+			uint32_t statusCode;
 			std::unordered_map<std::string, std::string> headers;
-            bool success;
-            std::string error;
-        };
-    
-        static Response get(std::string_view url);
-    };
-}
+		};
+
+		static ResponseBody parseResponse(std::string_view response);
+
+	public:
+		struct Response {
+			std::string body;
+			uint32_t statusCode;
+			std::unordered_map<std::string, std::string> headers;
+			bool success;
+			std::string error;
+		};
+
+		static Response get(std::string_view url);
+	};
+}// namespace squi
--- src/networking.cpp
+++ src/networking.cpp
@@ -1,4 +1,6 @@
 #include "networking.hpp"
+#include "asio/ssl.hpp"
+#include "skyr/url.hpp"
 #include <asio/error_code.hpp>
 #include <asio/ssl/verify_mode.hpp>
 #include <charconv>
@@ -6,8 +8,6 @@
 #include <skyr/v1/url.hpp>
 #include <sstream>
 #include <string>
-#include "asio/ssl.hpp"
-#include "skyr/url.hpp"
 
 using namespace squi;
 
@@ -15,109 +15,141 @@ asio::io_context Networking::ioContext;
 std::vector<std::thread> Networking::threads;
 
 Networking::ResponseBody Networking::parseResponse(std::string_view response) {
-    ResponseBody body;
-    const auto bodyStart = response.find("\r\n\r\n");
-    const auto header = response.substr(0, bodyStart);
-    const auto bodyStr = response.substr(bodyStart + 4);
+	ResponseBody ret;
+	// const auto bodyStart = response.find("\r\n\r\n");
+	// const auto header = response.substr(0, bodyStart);
+	// const auto bodyStr = response.substr(bodyStart + 4);
 
-    auto pos = header.begin();
-    do {
-        const auto lineEnd = std::find(pos, header.end(), '\r');
-        const auto line = header.substr(pos - header.begin(), lineEnd - pos);
-        
-        if (line.starts_with("HTTP/1.1")) {
-            const auto statusCode = line.substr(9, 3);
-            body.statusCode = std::stoi(std::string{statusCode});
-        } else {
-            const auto colon = line.find(':');
-            const auto key = line.substr(0, colon);
-            const auto value = line.substr(colon + 2);
-            body.headers.emplace(key, value);
-        }
+	auto cursor = response.begin();
+	do {
+		const auto lineEnd = std::find(cursor, response.end(), '\r');
+		const auto line = response.substr(cursor - response.begin(), lineEnd - cursor);
 
-        if (lineEnd == header.end()) break;
-        pos = lineEnd + 2;
-    } while (pos != header.end());
+		if (line.starts_with("HTTP/1.1")) {
+			const auto statusCode = line.substr(9, 3);
+			ret.statusCode = std::stoi(std::string{statusCode});
+		} else {
+			const auto colon = line.find(':');
+			const auto key = line.substr(0, colon);
+			// An empty key means we've reached the body
+			if (key.empty()) {
+				cursor = lineEnd + 2;
+				break;
+			}
+			const auto value = line.substr(colon + 2);
+			ret.headers.emplace(key, value);
+		}
 
-    body.body = std::string{bodyStr};
+		cursor = lineEnd + 2;
+	} while (cursor != response.end());
 
-    return body;
+	const auto body = response.substr(cursor - response.begin());
+
+	if (ret.headers.contains("Content-Length")) {
+		const auto contentLength = ret.headers["Content-Length"];
+		const auto length = std::stoi(std::string{contentLength});
+		ret.body = body.substr(0, length);
+	} else if (ret.headers.contains("Transfer-Encoding")) {
+		const auto encoding = ret.headers["Transfer-Encoding"];
+		if (encoding == "chunked") {
+			std::stringstream ss;
+			auto chunkStart = body.begin();
+			while (true) {
+				const auto chunkSizeEnd = std::find(chunkStart, body.end(), '\r');
+				const auto chunkSizeStr = body.substr(chunkStart - body.begin(), chunkSizeEnd - chunkStart);
+				size_t chunkSize;
+				std::from_chars(chunkSizeStr.data(), chunkSizeStr.data() + chunkSizeStr.size(), chunkSize, 16);
+				if (chunkSize == 0) break;
+				const auto chunkEnd = chunkSizeEnd + 2 + chunkSize;
+				ss << body.substr(chunkSizeEnd - body.begin() + 2, chunkSize);
+				chunkStart = chunkEnd + 2;
+			}
+			ret.body = ss.str();
+		} else {
+			std::cout << "Unknown transfer encoding: " << encoding << std::endl;
+		}
+	}
+
+	return ret;
 }
 
 Networking::Response Networking::get(std::string_view url) {
-    const auto parsedUrl = skyr::url{url};
-    bool isHttps = parsedUrl.protocol().starts_with("https");
-    asio::ip::tcp::resolver resolver(ioContext);
-    asio::ip::tcp::socket socket(ioContext);
-    asio::ssl::context context(asio::ssl::context::method::tls_client);
-    asio::ssl::stream<asio::ip::tcp::socket&> stream(socket, context);
-    asio::error_code ec;
-    const auto protocol = parsedUrl.protocol().substr();
-    auto endpoint = resolver.resolve(parsedUrl.host(), protocol.substr(0, protocol.size() - 1), ec);
-    if (ec) {
-        return Response{
-            .body = "",
-            .success = false,
-            .error = std::format("Failed to resolve {}: {}", parsedUrl.host(), ec.message()),
-        };
-    }
+	const auto parsedUrl = skyr::url{url};
+	bool isHttps = parsedUrl.protocol().starts_with("https");
+	asio::ip::tcp::resolver resolver(ioContext);
+	asio::ip::tcp::socket socket(ioContext);
+	asio::ssl::context context(asio::ssl::context::method::tls_client);
+	asio::ssl::stream<asio::ip::tcp::socket &> stream(socket, context);
+	asio::error_code ec;
+	const auto protocol = parsedUrl.protocol().substr();
+	auto endpoint = resolver.resolve(parsedUrl.host(), protocol.substr(0, protocol.size() - 1), ec);
+	if (ec) {
+		return Response{
+			.body = "",
+			.success = false,
+			.error = std::format("Failed to resolve {}: {}", parsedUrl.host(), ec.message()),
+		};
+	}
 
-    if (isHttps) {
-        context.set_verify_mode(asio::ssl::verify_peer);
-        context.set_default_verify_paths();
-    }
-    stream.lowest_layer().connect(endpoint->endpoint(), ec);
-    if (ec) {
-        return Response{
-            .body = "",
-            .success = false,
-            .error = std::format("Failed to connect to {}: {}", parsedUrl.host(), ec.message()),
-        };
-    }
+	if (isHttps) {
+		context.set_verify_mode(asio::ssl::verify_peer);
+		context.set_default_verify_paths();
+	}
+	stream.lowest_layer().connect(endpoint->endpoint(), ec);
+	if (ec) {
+		return Response{
+			.body = "",
+			.success = false,
+			.error = std::format("Failed to connect to {}: {}", parsedUrl.host(), ec.message()),
+		};
+	}
 
-    SSL_set_tlsext_host_name(stream.native_handle(), parsedUrl.host().data());
+	SSL_set_tlsext_host_name(stream.native_handle(), parsedUrl.host().data());
 
-    stream.handshake(asio::ssl::stream_base::client, ec);
-    if (ec) {
-        return Response{
-            .body = "",
-            .success = false,
-            .error = std::format("Failed to handshake with {}: {}", parsedUrl.host(), ec.message()),
-        };
-    }
+	stream.handshake(asio::ssl::stream_base::client, ec);
+	if (ec) {
+		return Response{
+			.body = "",
+			.success = false,
+			.error = std::format("Failed to handshake with {}: {}", parsedUrl.host(), ec.message()),
+		};
+	}
 
 	std::stringstream ss;
-    ss << parsedUrl.pathname();
-    if (!parsedUrl.search_parameters().empty()) ss << "?" << parsedUrl.search_parameters().to_string();
+	ss << parsedUrl.pathname();
+	if (!parsedUrl.search_parameters().empty()) ss << "?" << parsedUrl.search_parameters().to_string();
 
-    stream.write_some(asio::buffer(std::format("GET {} HTTP/1.1\r\nHost: {}\r\nAccept: Accept: */*\r\nConnection: close\r\n\r\n", ss.str(), parsedUrl.host())), ec);
+	stream.write_some(asio::buffer(std::format("GET {} HTTP/1.1\r\nHost: {}\r\nAccept: Accept: */*\r\nConnection: close\r\n\r\n", ss.str(), parsedUrl.host())), ec);
 	if (ec) {
-        return Response{
-            .body = "",
-            .success = false,
-            .error = std::format("Failed to send request to {}: {}", parsedUrl.host(), ec.message()),
-        };
-    }
+		return Response{
+			.body = "",
+			.success = false,
+			.error = std::format("Failed to send request to {}: {}", parsedUrl.host(), ec.message()),
+		};
+	}
 
-    std::string response;
-    std::array<char, 10 * 1024> buffer{};
-    while (true) {
-        size_t len = stream.read_some(asio::buffer(buffer), ec);
-        if (!ec) {
-            // printf("Got %zu bytes\n", len);
-            response.append(buffer.data(), len);
-        } else {
-            break;
-        }
-    }
+	std::string response;
+	std::array<char, 10 * 1024> buffer{};
+	while (true) {
+		size_t len = stream.read_some(asio::buffer(buffer), ec);
+		if (!ec) {
+			// printf("Got %zu bytes\n", len);
+			// std::cout << std::string_view{buffer.data(), len} << std::endl;
+			response.append(buffer.data(), len);
+		} else {
+			break;
+		}
+	}
 
-    auto body = parseResponse(response);
+	// std::cout << response << std::endl;
 
-    return Response{
-        .body = body.body,
-        .statusCode = body.statusCode,
-        .headers = body.headers,
-        .success = true,
-        .error = "",
-    };
+	auto body = parseResponse(response);
+
+	return Response{
+		.body = body.body,
+		.statusCode = body.statusCode,
+		.headers = body.headers,
+		.success = true,
+		.error = "",
+	};
 }
