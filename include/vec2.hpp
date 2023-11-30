43dd376d112ac84fb2daf1985d9fa4f567e724b9
Squizel <iustin224@gmail.com>
2023-11-30 16:20:00 +0200

    Various vec2 changes

--- include/vec2.hpp
+++ include/vec2.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <print>
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include "DirectXMath.h"
 #include "cmath"
+#include <limits>
 #include <ostream>
 
 namespace squi {
@@ -10,9 +20,20 @@ namespace squi {
 		float y;
 
 		constexpr vec2() noexcept : x(0), y(0) {}
-		vec2(float x, float y) noexcept : x(x), y(y) {}
-		vec2(float xy) noexcept : x(xy), y(xy) {}
-		vec2(const DirectX::XMFLOAT2 &vec) noexcept : x(vec.x), y(vec.y) {}
+		constexpr vec2(float x, float y) noexcept : x(x), y(y) {}
+		constexpr vec2(float xy) noexcept : x(xy), y(xy) {}
+		constexpr vec2(const DirectX::XMFLOAT2 &vec) noexcept : x(vec.x), y(vec.y) {}
+		static constexpr vec2 infinity() noexcept {
+			return vec2{
+				// I hope the person that created the max macro has a good day...
+				(std::numeric_limits<float>::max)(),
+				(std::numeric_limits<float>::max)(),
+			};
+		}
+
+		[[nodiscard]] constexpr float length() const noexcept {
+			return std::sqrtf(x * x + y * y);
+		}
 
 		[[nodiscard]] inline vec2 operator+(const vec2 &other) const {
 			return {x + other.x, y + other.y};
@@ -78,6 +99,10 @@ namespace squi {
 		friend std::ostream &operator<<(std::ostream &os, const vec2 &vec) {
 			os << "vec2(" << vec.x << ", " << vec.y << ")";
 			return os;
+		}
+
+		void print() const {
+			std::println("vec2({}, {})", x, y);
 		}
 	};
 }// namespace squi
