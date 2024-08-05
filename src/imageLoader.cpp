b64f57b2bf8ff2118f578b3cdfa7d655dae6d908
Squizel <iustin224@gmail.com>
2024-08-06 00:26:10 +0300

    Disable Sail logging

--- src/imageLoader.cpp
+++ src/imageLoader.cpp
@@ -1,12 +1,19 @@
 #include "imageLoader.hpp"
 
-#include "sail-c++/image_input.h"
-#include "stdexcept"
 #include "cstring"
+#include "sail-c++/image_input.h"
+#include "sail/sail-common/log.h"
+#include "stdexcept"
+
 
 using namespace squi;
 
 ImageLoadRes squi::loadImageInto(unsigned char *data, size_t length) {
+	[[maybe_unused]] static bool disableSailLogging = []() {
+		sail_set_log_barrier(SailLogLevel::SAIL_LOG_LEVEL_SILENCE);
+		return true;
+	}();
+
 	sail::image_input img_input{data, length};
 	auto img = img_input.next_frame();
 
@@ -30,5 +37,5 @@ ImageLoadRes squi::loadImageInto(unsigned char *data, size_t length) {
 	ret.data.resize(static_cast<size_t>(ret.width) * ret.height * ret.channels);
 	std::memcpy(ret.data.data(), res.pixels(), static_cast<size_t>(ret.width) * ret.height * ret.channels);
 
-    return ret;
+	return ret;
 }
