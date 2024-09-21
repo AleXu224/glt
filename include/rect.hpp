518d1456335849f337ecd71668f93f38fc403d35
Squizel <iustin224@gmail.com>
2024-09-21 16:31:56 +0300

    Cleanup

--- include/rect.hpp
+++ include/rect.hpp
@@ -10,37 +10,37 @@ namespace squi {
 		float bottom;
 
 		Rect(vec2 topLeft, vec2 bottomRight);
-		[[nodiscard]] inline static Rect fromPosSize(vec2 position, vec2 size) {
+		[[nodiscard]] static Rect fromPosSize(vec2 position, vec2 size) {
 			return {position, position + size};
 		}
 
-		[[nodiscard]] inline float width() const {
+		[[nodiscard]] float width() const {
 			return right - left;
 		}
-		[[nodiscard]] inline float height() const {
+		[[nodiscard]] float height() const {
 			return bottom - top;
 		}
 		[[nodiscard]] vec2 size() const {
 			return {width(), height()};
 		}
 
-		[[nodiscard]] inline vec2 getTopLeft() const {
+		[[nodiscard]] vec2 getTopLeft() const {
 			return {left, top};
 		}
 
-		inline void offset(vec2 offset) {
+		void offset(vec2 offset) {
 			left += offset.x;
 			right += offset.x;
 			top += offset.y;
 			bottom += offset.y;
 		}
 
-		[[nodiscard]] inline bool contains(const vec2 &position) const {
+		[[nodiscard]] bool contains(const vec2 &position) const {
 			return position.x >= left && position.x < right && position.y >= top && position.y < bottom;
 		}
 
-		[[nodiscard]] inline bool intersects(const Rect &other) const {
-			return !(left > other.right || right < other.left || top > other.bottom || bottom < other.top);
+		[[nodiscard]] bool intersects(const Rect &other) const {
+			return left <= other.right && right >= other.left && top <= other.bottom && bottom >= other.top;
 		}
 
 		Rect &inset(const float &distance) {
