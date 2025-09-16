d7fe9630bafdb93153e69b576d2ac95b6549c383
Squizel <iustin224@gmail.com>
2025-09-16 23:55:59 +0300

    Row, column

---
+++ include/widgets/row.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "core/core.hpp"
+#include "widgets/flex.hpp"
+
+namespace squi {
+	struct Row : StatelessWidget {
+		using Alignment = Flex::Alignment;
+		// Args
+		Key key;
+		Args widget{};
+		Alignment crossAxisAlignment = Alignment::start;
+		float spacing = 0.f;
+		Children children{};
+
+		[[nodiscard]] Child build(const Element &) const {
+			return Flex{
+				.widget = widget,
+				.direction = Axis::Horizontal,
+				.crossAxisAlignment = crossAxisAlignment,
+				.spacing = spacing,
+				.children = children,
+			};
+		}
+	};
+}// namespace squi
